f0cd19169b9d4f2b682887f9ac3fa96c567e72ce
Numan <unitednuman@gmail.com>
2025-06-16 17:16:03 +0500

    updates

--- main/blog_services/serializer.py
+++ main/blog_services/serializer.py
@@ -15,12 +15,12 @@ class PublisherSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ArticlesSerializer(serializers.ModelSerializer):
+    # reporter = ReporterSerializer(read_only=True)
+    # publisher = PublisherSerializer(read_only=True, many=True)
     class Meta:
         model = Articles
         fields = '__all__'
-        # fields = ('title', 'content')
-        # read_only_fields = ('id',)
-        # write_only_fields = ('title',)
+
     def to_representation(self, instance):
         data = super(ArticlesSerializer,self).to_representation(instance)
         if reporter := instance.reporter:
