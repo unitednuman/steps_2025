07976dd058a464a5ebd0a8de69e911ca2ab90905
Numan <unitednuman@gmail.com>
2025-06-17 16:40:06 +0500

    fifth lecture

--- main/blog_services/views.py
+++ main/blog_services/views.py
@@ -2,13 +2,35 @@ from django.shortcuts import render
 from .models import Articles, Reporter , Publisher
 from .serializer import ArticlesSerializer , ReporterSerializer , PublisherSerializer
 from rest_framework import generics
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 # Create your views here.
 
 class ArticlesListView(generics.ListCreateAPIView):
     serializer_class = ArticlesSerializer
     queryset = Articles.objects.all()
+    # icontans , contains , iexat how to impliment
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(name='title',
+                              in_ = openapi.IN_QUERY,description='Article Tile filter',
+                              type = openapi.TYPE_STRING),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        filters = {}
+        title = request.GET.get('title')
+        if title:
+             filters.update({'title': title})
+        if filters:
+            queryset = self.queryset.filter(**filters)
+        else:
+            queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
 
 
 class ArticlesUpdateView(generics.RetrieveUpdateDestroyAPIView):
--- main/main/settings.py
+++ main/main/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'blog_services'
+    'blog_services',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +82,10 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
